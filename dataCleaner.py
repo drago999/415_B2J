c5cebcfc5ba0fb22356e27fc3749cf178eebf00e
drago999 <dragunity@frontier.com>
2023-10-01 19:59:19 -0700

    Update dataCleaner.py

--- dataCleaner.py
+++ dataCleaner.py
@@ -41,7 +41,8 @@ think about what you need to do?
 do we want to eliminate certain data points?
 """
 
-print("\n\ncleaning start\n\n")
+#print("\n\ncleaning start\n\n")
+print("\n\nremove unwanted columns\n\n")
 
 #data cleanse addresses
 addresses.drop(["note", "sourceID", "valid_until", "address"], axis = 1, inplace=True)
@@ -68,13 +69,14 @@ relationships.drop(["sourceID"], axis = 1, inplace=True)
 print("\n\nreduction done\n\n")
 
 #look around
+"""
 print("\n\n")
 temp = addresses.loc[addresses["node_id"] == 120004328]
-print(addresses.loc[addresses["node_id"] == 120004328])
+print(temp.to_string())
 print("\n\n")
-
+"""
 #done playing
-
+"""
 # print(addresses.head())
 print(addresses.columns)
 print(len(addresses.columns))
@@ -93,15 +95,69 @@ print(len(others.columns))
 #print(relationships.head())
 print(relationships.columns)
 print(len(relationships.columns))
+"""
+
+print("\n\nstart removing dirty data\n\n")
+
+"""
+you need to itteratively go through and find "error prone data" 
+go through each line of data and if missing remove said node from everywhere (especiallly relationships)
+go until you reach the end
+"""
+print("WARNING SLOW DATA PROCESSING")
+"""
+#name.dropna() #drops rows wehere at least 1 element is missing
+missing_addresses = addresses.isnull() #can put into address[] for info
+#addresses[missing_addresses].to_csv("missing-addresses.csv", mode = 'w', index = False)
+missing_entities = entities.isnull()
+#entities[missing_entities].to_csv("missing-entities.csv")
+missing_intermediaries = intermediaries.isnull()
+#intermediaries[missing_intermediaries].to_csv("missing-intermediaries.csv", mode = 'w', index = False)
+missing_officers = officers.isnull()
+#officers[missing_officers].to_csv("missing-officers.csv", mode = 'w', index = False)
+missing_others = others.isnull()
+#others[missing_others].to_csv("missing-others.csv", mode = 'w', index = False)
+missing_relationships = relationships.isnull()
+#relationships[missing_relationships].to_csv("missing-relationships.csv", mode = 'w', index = False)
+"""
+print("\nFOUND ALL MISSING DATA\nREMOVING ALL NODES MISSING DATA\n")
+#need to find node_id in each and remove said node_id from all
+
+#temp = entities[entities.loc("node_id" = node_id)] #repeat for all others
+#go through you missing_x until all of missing x is removed from x,y,z
+#if(missing_addresses["node_id"][x] == True)
+
+#remove everything that is missing?
+#then go through the relationships remaining and remove all missing links?
+clean_addresses = addresses.dropna()
+clean_entities = entities.dropna()
+clean_intermediaries = intermediaries.dropna()
+clean_officers = officers.dropna()
+clean_others = others.dropna()
+###data cleaned outside of relationships###
+print("\nCHECKING RELATIONSHIPS\n")
+for x in range(len(relationships)):
+    print(x, " out of ", len(relationships))
+
+
+
+
+
+
+
+
+
+#### completed cleaning
 
 print("\n\ncleaning done\n\n")
 
+
 print("begin printing new .csv\n")
-addresses.to_csv("reduced-addresses.csv")
-entities.to_csv("reduced-entities.csv")
-intermediaries.to_csv("reduced-intermediaries.csv")
-officers.to_csv("reduced-officers.csv")
-others.to_csv("reduced-others.csv")
-relationships.to_csv("reduced-relationships.csv")
+clean_addresses.to_csv("reduced-addresses.csv", mode = 'w', index = False)
+clean_entities.to_csv("reduced-entities.csv")
+clean_intermediaries.to_csv("reduced-intermediaries.csv", mode = 'w', index = False)
+clean_officers.to_csv("reduced-officers.csv", mode = 'w', index = False)
+clean_others.to_csv("reduced-others.csv", mode = 'w', index = False)
+relationships.to_csv("reduced-relationships.csv", mode = 'w', index = False)
 
 print("\n\ndone\n\n")
